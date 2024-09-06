be52ab1d1a21bb74b44103b0f55111ed55458652
IgnaDev <ignacio.n.basilio.b@gmail.com>
2024-09-06 20:08:46 -0300

    hola mundo, variables y constantes

--- Java/Bases/2-Datos/DataTypes.js
+++
@@ -1,7 +0,0 @@
-public class DataTypes {
-  public static void main(String[] args) {
-    // Int es un tipo de dato que representa un número entero, llega hasta 2,147,483,647.
-    int n = 1234567890;
-  }
-.
-}
